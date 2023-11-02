a6f20b092223d2d43d9c083872b016c7007633df
Tina Bruce <tinabruce42@hotmail.com>
2023-11-02 20:13:34 +0100

    Handle neew issue form submission errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import { TextArea, TextField, Button } from '@radix-ui/themes';
+import { TextField, Button, Callout } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm  } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 // define the shape of our form - what fiels they have and their types
 interface IssueForm {
@@ -13,30 +14,45 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+    const [error, setError] = useState('');
     const router = useRouter();
+
     // useForm returns an object, which when restructed 
     // provides the register function
     // using register to register the inputfields with react hook form, so it can keep track of them
     const { register, control, handleSubmit } = useForm<IssueForm>();
 
     return (
-        <form 
-          className='max-w-xl space-y-3' 
-          onSubmit={handleSubmit(async (data) => { 
-            await axios.post('/api/issues', data);
-            router.push('/issues')
-          })}>
-            NewIssuePage
-            <TextField.Root>                         {/*use the spread op so that we get access to all the properties the function comes with */}
-                <TextField.Input placeholder='Title' {...register('title')}/>
-            </TextField.Root>                      
-            <Controller             
-              name='description'
-              control={control}
-              render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>} 
-            /> {/* above way not supported for SimpleMDE, have to use the controller component in react-hook-form */}
-            <Button>Submit new issue</Button>
-        </form>
+        <div className='max-w-xl'>
+            {error && 
+                <Callout.Root className='mb-5'>
+                    {/* <Callout.Icon>
+                        <InfoCircledIcon />
+                    </Callout.Icon> */}
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>}
+            <form 
+                className='space-y-3' 
+                onSubmit={handleSubmit(async (data) => { 
+                    try {
+                        await axios.post('/api/issues', data);
+                        router.push('/issues');
+                    } catch (error) {
+                        setError('An unknown error occured.')
+                    }
+                })}>
+                NewIssuePage
+                <TextField.Root>                         {/*use the spread op so that we get access to all the properties the function comes with */}
+                    <TextField.Input placeholder='Title' {...register('title')}/>
+                </TextField.Root>                      
+                <Controller             
+                    name='description'
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>} 
+                /> {/* above way not supported for SimpleMDE, have to use the controller component in react-hook-form */}
+                <Button>Submit new issue</Button>
+            </form>
+        </div>
     )
 }
 
