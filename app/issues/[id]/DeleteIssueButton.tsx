d21717952ef6427e32382aafa046ae9522afe03e
Tina Bruce <tinabruce42@hotmail.com>
2024-01-18 16:12:30 +0100

    Add spinner and disable button when issue is being deleted

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -7,12 +8,17 @@ import { useState } from "react";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const deleteIssue = async () => {
     try {
+      setIsDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push("/issues");
       router.refresh();
+      setIsDeleting(false);
     } catch (error) {
+      setIsDeleting(false);
       setError(true);
     }
   };
@@ -21,7 +27,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="ruby">Delete issue</Button>
+          <Button color="ruby" disabled={isDeleting}>Delete issue
+          {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
 
         <AlertDialog.Content>
