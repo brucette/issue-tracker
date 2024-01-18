519372a070ec9a6548ad505526dfd90de0f55695
Tina Bruce <tinabruce42@hotmail.com>
2024-01-18 15:07:59 +0100

    Build an API endpoint for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,3 +30,26 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue, { status: 200 })
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string }}) {
+  // const body = await request.json();
+  // const validation = issueSchema.safeParse(body);
+
+  // if (!validation.success) {
+  //   return NextResponse.json(validation.error.format(), { status: 400 })
+  // }
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) }
+  })
+
+  if (!issue) {
+    return NextResponse.json({ error: 'Invalid issue'}, { status: 404 })
+  }
+
+  await prisma.issue.delete({
+    where: { id: issue.id }
+  })
+
+  return NextResponse.json({});
+}
