2613866e559ed392094723a91e6ed060267f5d8f
Tina Bruce <tinabruce42@hotmail.com>
2024-01-30 15:31:57 +0100

    Secure the application

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,38 +1,43 @@
-import prisma from '@/prisma/client';
-import { Box, Grid, Flex } from '@radix-ui/themes';
-import delay from 'delay';
-import { notFound } from 'next/navigation';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
-import DeleteIssueButton from './DeleteIssueButton';
+import prisma from "@/prisma/client";
+import { Box, Grid, Flex } from "@radix-ui/themes";
+import delay from "delay";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
 const IssueDetailsPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
+
   const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(params.id)}
+    where: { id: parseInt(params.id) },
   });
 
-  if (!issue)
-    notFound();
+  if (!issue) notFound();
 
   await delay(900);
 
   return (
     <Grid columns={{ initial: "1", sm: "5" }} gap="5">
-      <Box className='md:col-span-4'>
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction='column' gap='4'>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
-  )
-}
+  );
+};
 
 export default IssueDetailsPage;
