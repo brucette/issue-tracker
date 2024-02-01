06934c294a3d6e4c9900f9ff470e99092bfbed40
Tina Bruce <tinabruce42@hotmail.com>
2024-02-01 14:01:24 +0100

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,12 +1,28 @@
-import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import delay from 'delay';
 import {IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
+import prisma from '@/prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(500)
+interface Props {
+  searchParams: { status: Status }
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  
+  // Validate the type of status chosen
+  const status = statuses.includes(searchParams.status) 
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status
+    }
+  });
+  await delay(200)
 
   return (
     <div>
@@ -21,7 +37,6 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map(issue => (
-            
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>
